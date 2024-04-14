09c2002b9abd9a39dd7d3c3034fb54e2c814b7ff
Jalolkhon-mdist <164335784+Jalolkhon-mdist@users.noreply.github.com>
2024-04-14 18:43:23 +0000

    .....................

--- src/layouts/index.ts
+++
@@ -1,7 +0,0 @@
-import Navigation from "./Navigation";
-import Searchbar from "./Searchbar";
-import Filter from "./Filter";
-import MainLayout from "./MainLayout";
-import SidebarList from "./SidebarList";
-
-export { Navigation, Searchbar, Filter, MainLayout, SidebarList };
