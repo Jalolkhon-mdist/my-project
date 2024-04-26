9c98d649f4de2f08de27882cfa597847a1933290
Jalolkhon-mdist <164335784+Jalolkhon-mdist@users.noreply.github.com>
2024-04-26 15:34:58 +0000

    .....

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,7 @@
-import VacancyCard from "./VacancyCard";
 import Comment from "./Comment";
 import CommentEditor from "./CommentEditor";
 import Logo from "./Logo";
 import PostCard from "./PostCard";
 import Dropdown from "./Dropdown";
 
-export { VacancyCard, Comment, CommentEditor, Logo, PostCard, Dropdown };
+export { Comment, CommentEditor, Logo, PostCard, Dropdown };
